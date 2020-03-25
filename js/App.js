0cca32ba2b627311834455275cff0cd76e59a313
Kany <kemuel.kany@live.fr>
2020-03-25 12:17:37 +0100

    V1 fonctionnel avec b64 version

---
+++ js/App.js
@@ -0,0 +1,43 @@
+'user strict';
+
+(async () => {
+ const app = document.querySelector('#app main');
+
+  const result = await fetch('/data/spacex.json');
+  const data = await result.json();
+  
+  
+  const cards = data.map(item => {
+    const constructor = document.createElement('div');
+    constructor.innerHTML = `
+      <section class="card">
+        <header>
+          <div class="placeholder"></div>
+          <figure><img src="" alt=""></figure>
+        </header>
+        <main>
+          <h1></h1>
+          <p></p>
+        </main>
+      </section>
+    `;
+    const card = constructor.querySelector('.card');
+    initCard(card, item);
+
+    app.appendChild(card);
+    return card;
+  });
+
+})();
+
+function initCard(card, data) {
+  const placeholder = card.querySelector('.placeholder');
+  placeholder.style.cssText = `background-image: url(${data.placeholder})`;
+
+  const image = card.querySelector('img');
+  image.dataset.src = data.image;
+  image.alt = data.content.title;
+
+  // ...
+  
+}
