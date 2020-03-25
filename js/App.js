890efbf15c5b70d383cf2859e6b909af0f6ce480
Kany <kemuel.kany@live.fr>
2020-03-25 12:46:41 +0100

    part 1 fini, avec un IntersectionObserver

--- js/App.js
+++ js/App.js
@@ -12,8 +12,10 @@
     constructor.innerHTML = `
       <section class="card">
         <header>
+        <figure>
           <div class="placeholder"></div>
-          <figure><img src="" alt=""></figure>
+            <img src="" alt="">
+          </figure>
         </header>
         <main>
           <h1></h1>
@@ -28,17 +30,43 @@
     return card;
   });
 
-  setTimeout(() => {
-    cards.forEach(card => {
-      const cover = card.querySelector('img');
-      cover.src = cover.dataset.src;
+  // setTimeout(() => {
+  //   cards.forEach(card => {
+  //     const cover = card.querySelector('img');
+  //     cover.src = cover.dataset.src;
 
-      const placeholder = card.querySelector('.placeholder');
-      placeholder.classList.add('fade');
+  //     const placeholder = card.querySelector('.placeholder');
+  //     placeholder.classList.add('fade');
+  //   });
+  // }, 2000);
+
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+   */
+  const options = {
+    rootMarging : '0px 0px 0px 0px'
+  };
+
+  const callback = (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const image = entry.target;
+        image.src = image.dataset.src;
+
+        image.onload = () => {
+          image.parentNode.querySelector('.placeholder').classList.add('fade');
+        }
+      }
     });
-  }, 2000);
+  }
 
+  const io = new IntersectionObserver(callback, options);
+  cards.forEach(card => {
+    const image = card.querySelector('img');
+    io.observe(image);
+  });
 })();
+
 
 function initCard(card, data) {
   const placeholder = card.querySelector('.placeholder');
